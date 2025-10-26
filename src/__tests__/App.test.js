c8bdf8cfe30e810c606e5c7c4af94f2cf7638b8d
David <davidkuron1996@gmail.com>
2025-10-26 17:49:53 +0300

    add this files

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,24 @@
 import { render, screen } from "@testing-library/react";
 import App from "../App";
-
 import '@testing-library/jest-dom';
 
 // Code tests here
+test("renders Hello, World! heading", () => {
+    render(<App />);
+    const headingElement = screen.getByText(/Hello, World!/i);
+    expect(headingElement).toBeInTheDocument();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
